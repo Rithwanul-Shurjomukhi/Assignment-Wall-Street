32db9dbf567e559a05cc3f13cc5e6a656f869698
T.M.Rithwanul Islam <43297466+Rithwanul@users.noreply.github.com>
2024-05-08 15:27:06 +0600

    Initial commit

---
+++ src/main/java/com/assignment/wallstreet/AssignmentWallStreetApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.wallstreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentWallStreetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentWallStreetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/wallstreet/AssignmentWallStreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.wallstreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentWallStreetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
